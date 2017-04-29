76130ec9a3f8ca278b8b6a0bcd4096d4384d4bc5
Rebecca Way <rebeccaway2016@u.northwestern.edu>
2017-04-29 10:44:39 -0500

    removed css applied to elements with css applied to classes on those elements. Also rearranged the directory layout.

---
+++ js/acnl_script.js
@@ -0,0 +1,40 @@
+// document.getElementById('namebox').addEventListener("keyup", function(event) {
+//     event.preventDefault();
+//     if (event.keyCode == 13) {
+//         document.getElementById("#namebutton").click();
+//     }
+// });
+
+$(document).ready(function(){
+    $('#namebox').keypress(function(e){
+      if(e.keyCode==13)
+      $('#namebutton').click();
+    });
+});
+
+function db_search(item, db) {
+    for(var x=0; x<db.length;x++){
+        if(db[x].Villager.toLowerCase() == item.toLowerCase()){
+          return x;
+        }
+      }
+    return -1;
+}
+
+function set_coffee() {
+  name = document.getElementById('namebox').value;
+  result = db_search(name, cafe_db);
+  if(result>=0){
+    document.getElementById("villager_name").innerHTML="Here's " +
+      cafe_db[result].Villager + "'s coffee order!";
+    document.getElementById("beans_type").innerHTML=cafe_db[result].Beans;
+    document.getElementById("milk_type").innerHTML=cafe_db[result].Milk;
+    document.getElementById("sugar_type").innerHTML=cafe_db[result].Sugar;
+  } else{
+    document.getElementById("villager_name").innerHTML="Please enter a villager's " +
+    "name into the text box, then press the button (check spelling!):";
+    document.getElementById("beans_type").innerHTML="";
+    document.getElementById("milk_type").innerHTML="";
+    document.getElementById("sugar_type").innerHTML="";
+  }
+}
