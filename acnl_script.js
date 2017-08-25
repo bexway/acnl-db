bb5bda920a773ef760a6ca21db8f7c73888a3414
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-08-25 10:48:25 -0500

    master - switched getElementById calls to jQuery

--- acnl_script.js
+++ acnl_script.js
@@ -15,19 +15,19 @@ function db_search(item, db) {
 }
 
 function set_coffee() {
-  name = document.getElementById('namebox').value;
+  name = $('#namebox').value;
   result = db_search(name, cafe_db);
   if(result>=0){
-    document.getElementById("villager_name").innerHTML="Here's " +
+    $("#villager_name").innerHTML="Here's " +
       cafe_db[result].Villager + "'s coffee order!";
-    document.getElementById("beans_type").innerHTML=cafe_db[result].Beans;
-    document.getElementById("milk_type").innerHTML=cafe_db[result].Milk;
-    document.getElementById("sugar_type").innerHTML=cafe_db[result].Sugar;
+    $("#beans_type").innerHTML=cafe_db[result].Beans;
+    $("#milk_type").innerHTML=cafe_db[result].Milk;
+    $("#sugar_type").innerHTML=cafe_db[result].Sugar;
   } else{
-    document.getElementById("villager_name").innerHTML="Please enter a villager's " +
+    $("#villager_name").innerHTML="Please enter a villager's " +
     "name into the text box, then press the button (check spelling!):";
-    document.getElementById("beans_type").innerHTML="";
-    document.getElementById("milk_type").innerHTML="";
-    document.getElementById("sugar_type").innerHTML="";
+    $("#beans_type").innerHTML="";
+    $("#milk_type").innerHTML="";
+    $("#sugar_type").innerHTML="";
   }
 }
